86961694b677df8c5435a629c226de317ca1e3c7
Jens Leicht <jens.leicht@plus.ac.at>
2026-05-12 07:28:27 +0200

    Init

--- public/item.js
+++ public/item.js
@@ -1,7 +1,22 @@
 export default class Item {
-  constructor(summary) {
+  constructor(content, listener) {
+    this.element = document.createElement("li");
+    this.element.textContent = content;
+    this.deleteButton = document.createElement("button");
+    this.deleteButton.textContent = "delete";
+    this.element.append(this.deleteButton);
+    this.itemDeleteHandler = listener;
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.deleteButton.addEventListener("click",this.handleDelete);
+  }
+
+  handleDelete(event) {
+    this.element.remove();
+    this.itemDeleteHandler(this);
   }
 
   addToDOM(list) {
+    list.append(this.element);
   }
 }
--- public/list.js
+++ public/list.js
@@ -2,6 +2,38 @@ import Item from "./item.js";
 
 class List {
   constructor() {
+    this.form = document.forms.additem;
+    this.input = this.form.item;
+    this.list = document.querySelector("#list");
+    this.clearButton = document.querySelector("#clearlist");
+    this.items = [];
+
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+
+    this.form.addEventListener("submit", this.handleAdd);
+    this.clearButton.addEventListener("click", this.handleClear);
   }
+
+  handleAdd(event) {
+    event.preventDefault();
+
+    let item = new Item(this.input.value, this.handleRemove);
+    this.items.push(item);
+    item.addToDOM(this.list);
+    this.form.reset();
+  }
+
+  handleRemove(item) {
+    this.items.splice(this.items.indexOf(item),1);
+  }
+
+  handleClear(event) {
+    this.list.textContent = "";
+    this.items = []
+  }
+
 }
-new List();
+
+window.list = new List();
